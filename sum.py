e1e0bd8514046782f150ed9d4885de670b7885c4
Nesrine <sirine.benncef@gmail>
2024-11-06 12:35:34 +0100

    added py and dockerfile files

--- sum.py
+++ sum.py
@@ -1,16 +1,20 @@
 import sys
-# Verifier si deux arguments sont fournis
-if len(sys.argv)!= 3:
-print(" Erreur : Deux arguments sont n ece s s a i r e s .")
-sys.exit (1)
-# Interprter les arguments
+
+# Vérifier si deux arguments sont fournis
+if len(sys.argv) != 3:
+    print("Erreur : Deux arguments sont nécessaires.")
+    sys.exit(1)
+
+# Interpréter les arguments
 try:
-arg1 = float(sys.argv [1])
-arg2 = float(sys.argv [2])
-except ValueError :
-print(" Erreur :Les arguments doivent etre des nombres .")
-sys.exit (1)
+    arg1 = float(sys.argv[1])
+    arg2 = float(sys.argv[2])
+except ValueError:
+    print("Erreur : Les arguments doivent être des nombres.")
+    sys.exit(1)
+
 # Calculer la somme
-resultat=arg1 + arg2
-# Afficher le resultat
+resultat = arg1 + arg2
+
+# Afficher le résultat
 print(resultat)
